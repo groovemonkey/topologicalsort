bb479967bb896156cbc1ed025c9dfb31bebe6d27
David Cohen <dcohen@hashicorp.com>
2023-09-30 12:52:04 -0400

    rewrite implementation to use generics, add some tests for storing arbitrary structs as node data. Stuck on a hard-to-repo ordering bug.

--- topologicalsort_test.go
+++ topologicalsort_test.go
@@ -1,17 +1,18 @@
 package topologicalsort
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
 
-func graphWithVertices(vertices map[string][]string) *Graph {
-	graph := NewGraph()
+func graphWithVerticesDUMMYDATA[T any](vertices map[string][]string, sampleData T) *Graph[T] {
+	graph := NewGraph(sampleData)
 
 	// Create vertices
 	for k := range vertices {
 		// uses dummy values
-		graph.RegisterVertex(k, "")
+		graph.RegisterVertex(k, sampleData)
 	}
 
 	// add edges
@@ -30,18 +31,21 @@ func TestGraph_TopologicalSort(t *testing.T) {
 		name string
 		// adjacency_list is used to build the test graph -- vertices and all of their dependencies
 		adjacency_list map[string][]string
+		dummyData      any
 		want           []string
 		wantErr        bool
 	}{
 		{
 			name:           "A graph with no vertices is already sorted.",
 			adjacency_list: map[string][]string{},
+			dummyData:      "",
 			want:           []string{},
 			wantErr:        false,
 		},
 		{
 			name:           "A graph with one vertex is already sorted.",
 			adjacency_list: map[string][]string{"sorted": {}},
+			dummyData:      "",
 			want:           []string{"sorted"},
 			wantErr:        false,
 		},
@@ -60,20 +64,22 @@ func TestGraph_TopologicalSort(t *testing.T) {
 				"gcc":             {"libc"},
 				"libc":            {},
 			},
-			want:    []string{"libc", "gcc", "make", "build-essential"},
-			wantErr: false,
+			dummyData: "",
+			want:      []string{"libc", "gcc", "make", "build-essential"},
+			wantErr:   false,
 		},
 		{
 			name: "A graph with no cycles can be sorted.",
 			adjacency_list: map[string][]string{
+				"four":  {"three"},
 				"one":   {},
 				"two":   {"one"},
 				"three": {"two"},
-				"four":  {"three"},
 				"five":  {"four"},
 			},
-			want:    []string{"one", "two", "three", "four", "five"},
-			wantErr: false,
+			dummyData: "",
+			want:      []string{"one", "two", "three", "four", "five"},
+			wantErr:   false,
 		},
 		{
 			name: "A graph with a cycle triggers an error",
@@ -84,14 +90,92 @@ func TestGraph_TopologicalSort(t *testing.T) {
 				"four":  {"three", "two", "one"},
 				"five":  {"four", "three"},
 			},
-			want:    []string{},
-			wantErr: true,
+			dummyData: "",
+			want:      []string{},
+			wantErr:   true,
+		},
+		{
+			name: "Test Generics: a graph using integer data still works",
+			adjacency_list: map[string][]string{
+				"one": {},
+				"two": {"one"},
+			},
+			dummyData: 0,
+			want:      []string{"one", "two"},
+			wantErr:   false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			g := graphWithVertices(tt.adjacency_list)
+			g := graphWithVerticesDUMMYDATA(tt.adjacency_list, tt.dummyData)
 			got, err := g.TopologicalSort()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Graph.TopologicalSort() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !tt.wantErr && !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Graph.TopologicalSort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_TopographicSort_With_Arbitrary_Data(t *testing.T) {
+	type myTestType struct {
+		floob string
+		burb  int
+	}
+
+	tests := []struct {
+		name       string
+		graph_data map[*GraphNode[myTestType]][]string
+		want       []string
+		wantErr    bool
+	}{
+		{
+			name:       "A graph with no vertices is already sorted.",
+			graph_data: map[*GraphNode[myTestType]][]string{},
+			want:       []string{},
+			wantErr:    false,
+		},
+		{
+			name: "Graph nodes with struct type Data can still be Topographically sorted",
+			graph_data: map[*GraphNode[myTestType]][]string{
+				// node three (intentionally out of order)
+				{
+					Key: "three",
+					Data: myTestType{
+						floob: "dataforthree",
+						burb:  3,
+					},
+				}: {"two"},
+				// node one
+				{
+					Key: "one",
+					Data: myTestType{
+						floob: "dataforone",
+						burb:  1,
+					},
+				}: {},
+				// node two
+				{
+					Key: "two",
+					Data: myTestType{
+						floob: "datafortwo",
+						burb:  2,
+					},
+				}: {"one"},
+			},
+			want:    []string{"one", "two", "three"},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := NewGraphFromData(tt.graph_data)
+
+			got, err := g.TopologicalSort()
+			fmt.Println(fmt.Sprintf("%+v", g))
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Graph.TopologicalSort() error = %v, wantErr %v", err, tt.wantErr)
 				return
